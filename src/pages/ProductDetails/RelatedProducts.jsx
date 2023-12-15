8a2d6c67b34e03afa9f849deb6cc04e67fefd0f3
abdulsaboor10 <saboor.bhutta10@gmail.com>
2023-12-15 18:56:16 +0500

    completed product details ui

--- src/pages/ProductDetails/RelatedProducts.jsx
+++ src/pages/ProductDetails/RelatedProducts.jsx
@@ -5,6 +5,7 @@ const RelatedProducts = () => {
   return (
     <div className='mt-8'>
         <p className='text-[20px] font-medium'>Related Products</p>
+        
         <div className='mt-4 flex flex-wrap gap-6'>
             {
                 new Array(6).fill(0).map((_ , index) => {
